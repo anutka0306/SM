d7d7f94e8767b7bb43163e9ff75a9888062b3dc6
anutka0306 <anutka0306@mail.ru>
2021-06-21 15:39:25 +0300

    пробую отправку ajax

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -8,12 +8,13 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 
+
 class MailerController extends AbstractController
 {
     /**
      * @Route("/contact_form", name="contact_form")
      */
-    public function contact_form(MailerInterface $mailer): Response
+    public function contact_form(MailerInterface $mailer)
     {
         $email = (new Email())
             ->from('robot@my-side.online')
@@ -22,6 +23,8 @@ class MailerController extends AbstractController
             ->text('This is my test email')
             ->html('<p>Hello here</p>');
         $mailer->send($email);
+
+        return 'sended';
         /*return $this->render('mailer/index.html.twig', [
             'controller_name' => 'MailerController',
         ]);*/
