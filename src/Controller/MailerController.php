5c11113b8fb2cc50e33caf179149af6962cf95b4
anutka0306 <anutka0306@mail.ru>
2021-06-21 15:59:53 +0300

    test

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mailer\MailerInterface;
@@ -14,20 +15,14 @@ class MailerController extends AbstractController
     /**
      * @Route("/contact_form", name="contact_form")
      */
-    public function contact_form(MailerInterface $mailer)
+    public function contact_form(Request $request, MailerInterface $mailer)
     {
         $email = (new Email())
             ->from('robot@my-side.online')
             ->to('robot@my-side.online')
             ->subject('Test email')
             ->text('This is my test email')
-            ->html('<p>Сообщение со втаницы контакты:</p>
-<p>Имя отправителя: '.$_POST['user_name'] .'</p>
-<p>E-mail отправителя: '.$_POST['user_email'] .'</p>
-<p>Телефон отправителя: '.$_POST['user_phone'] .'</p>
-<p>Салон: '.$_POST['salon'] .'</p>
-<p>Сообщение: '.$_POST['comment'] .'</p>
-');
+            ->html('<p>Сообщение со втаницы контакты:</p>'.$request->get('some_var_name'));
         $mailer->send($email);
 
         return new Response('<p>Спасибо! Ваше сообщение отправлено.</p>');
