ceee5a889c8fcc51e47a44bce124d7c1e74b57eb
anutka0306 <anutka0306@mail.ru>
2021-06-22 12:14:30 +0300

    Форма на стр. контакты - готова

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -3,37 +3,120 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 
 class MailerController extends AbstractController
 {
+    private $validatorInterface;
+
+    public function __construct(ValidatorInterface $validatorInterface)
+    {
+       $this->validatorInterface = $validatorInterface;
+    }
+
     /**
      * @Route("/contact_form", name="contact_form")
      */
     public function contact_form(Request $request, MailerInterface $mailer)
     {
-        $email = (new Email())
-            ->from('robot@my-side.online')
-            ->to('robot@my-side.online')
-            ->subject('Test email')
-            ->text('This is my test email')
-            ->html('<p>Сообщение со втаницы контакты:</p>
-             <p>Имя отправителя: '.$request->get('user_name_contact') .'</p>
-<p>E-mail отправителя: '.$request->get('user_email_contact') .'</p>
-<p>Телефон отправителя: '.$request->get('user_phone_contact') .'</p>
-<p>Салон: '.$request->get('salon_contact') .'</p>
-<p>Сообщение: '.$request->get('comment_contact') .'</p>'
-            );
-        $mailer->send($email);
+        $errors = array();
+        $userName ='';
+        $userEmail = '';
+        $userPhone = '';
+        if(!$this->addEmail($request->get('user_email_contact'), $this->validatorInterface)){
+            $errors[] = 'Некорректный E-mail адрес';
+        }
+        if (!$this->addName($request->get('user_name_contact'), $this->validatorInterface)){
+            $errors[] = 'Имя должно содержать не меньше 2-х символов. Может содержать только русские буквы.';
+        }
+        if(!$this->addPhone($request->get('user_phone_contact'), $this->validatorInterface)){
+            $errors[] = 'Некорректный номер телефона';
+        }
+        else{
+            $userEmail = $this->addEmail($request->get('user_email_contact'), $this->validatorInterface);
+            $userName = $this->addName($request->get('user_name_contact'), $this->validatorInterface);
+            $userPhone = $this->addPhone($request->get('user_phone_contact'), $this->validatorInterface);
 
-        return new Response('<p>Спасибо! Ваше сообщение отправлено.</p>');
-        /*return $this->render('mailer/index.html.twig', [
-            'controller_name' => 'MailerController',
-        ]);*/
+        }
+        if(0 === count($errors)) {
+            $email = (new Email())
+                ->from('robot@my-side.online')
+                ->to('robot@my-side.online')
+                ->subject('Новое сообщение с сайта')
+                ->text('This is my test email')
+                ->html('<p>Сообщение со страницы контакты:</p>
+             <p>Имя отправителя: ' . $userName . '</p>
+<p>E-mail отправителя: ' . $userEmail . '</p>
+<p>Телефон отправителя: ' . $userPhone . '</p>
+<p>Салон: ' . $request->get('salon_contact') . '</p>
+<p>Сообщение: ' . $request->get('comment_contact') . '</p>'
+                );
+            $mailer->send($email);
+
+            return new JsonResponse(['success'=>'<p>Спасибо! Ваше сообщение отправлено.</p>']);
+        }else{
+            return new JsonResponse(['errors'=>$errors]);
+        }
+
     }
+
+    public function addEmail($email, ValidatorInterface $validator){
+        $emailConstraint = array(
+            new Assert\Email(),
+            new Assert\NotBlank(),
+        );
+        $errors = $validator->validate(
+          $email,
+          $emailConstraint
+        );
+
+        if(0 === count($errors)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function addName($name, ValidatorInterface $validator){
+        $nameConstraint = array(
+            new Assert\NotBlank(),
+            new Assert\Length(['min' => 2]),
+            new Assert\Regex(['pattern' => '/^[а-яёА-ЯЁ]+$/'])
+        );
+
+        $errors = $validator->validate(
+            $name,
+            $nameConstraint
+        );
+        if(0 === count($errors)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function addPhone($phone, ValidatorInterface $validator){
+        $phoneConstraint = array(
+          new Assert\NotBlank(),
+          new Assert\Regex(['pattern' => '/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/'])
+        );
+        $errors = $validator->validate(
+            $phone,
+            $phoneConstraint
+        );
+        if(0 === count($errors)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
